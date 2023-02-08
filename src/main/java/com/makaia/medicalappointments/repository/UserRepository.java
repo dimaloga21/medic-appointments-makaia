4c55bfdb46f559caa541b1a08a46be77f82f849a
dimaloga21 <123612879+dimaloga21@users.noreply.github.com>
2023-02-08 13:51:09 -0500

    Delete src/main/java/com/makaia/medicalappointments directory
    Se borra proyecto

--- src/main/java/com/makaia/medicalappointments/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.makaia.medicalappointments.repository;
-
-import com.makaia.medicalappointments.model.UserE;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<UserE, Integer> {
-}
--- src/main/java/com/makaia/medicalappointments/service/UserType.java
+++
@@ -1,6 +0,0 @@
-package com.makaia.medicalappointments.service;
-
-public enum UserType {
-
-    EPS, PARTICULAR, POLIZA
-}
